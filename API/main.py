952238476eb5af7819461fba257b0ab3e2de1ccf
LeshaTheBeginner <leshaprosratch@gmail.com>
2026-06-06 12:06:53 +0200

    ADD: First steps: Created README.md and created blueprint for the API

---
+++ API/main.py
@@ -0,0 +1,57 @@
+# /create POST
+# /edit POST
+# /delete POST
+# /getnotes
+# /star
+# /lastdeleted
+# /lastcreated
+# /lastused
+# /help
+# / 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/help")
+async def help():
+    return {"message": "help page but he have none"}
+
+@app.get("/create")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/edit")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/delete")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/getnotes")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/star")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/lastused")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/lastcreated")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/lastdeleted")
+async def root():
+    return {"message": "Hello lastdeleted"}
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NotepadApi
+/create -- Creates a note
+/edit -- Edits a note
+/delete -- Deletes a note
+/getnotes Returns all the notes
+/star Stars (Favorites) a note
+/lastdeleted Returns the last deleted note
+/lastcreated Returns the last created note
+/lastused -- Returns the last used note
+/help -- Help Page
+/ -- Hello World!
